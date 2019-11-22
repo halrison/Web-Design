db5258d05354db3284afb320f4285f9389fb8461
內爾娛樂科技 <halrison@outlook.com>
2019-11-23 00:34:02 +0800

    完成丙級第二題術科

---
+++ ClassC/WebC02/JavaScript.js
@@ -0,0 +1,11 @@
+﻿document.body.onload=function() {
+    var canvas = document.getElementsByTagName('canvas')[0], context = canvas.getContext('2d');
+    context.fillStyle = "white";
+    context.font="52px 標楷體";
+    context.shadowColor= "#333333";
+    context.shadowBlur= 15;
+    context.shadowOffsetX=10;
+    context.shadowOffsetY = 10;
+    context.textBaseline = 'center';
+    context.fillText("運動廣場連結網",canvas.width/4,canvas.height*2/3,364);
+};

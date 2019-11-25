5d85897cfada46c8bde422197f543a8868d97779
內爾娛樂科技 <halrison@outlook.com>
2019-11-25 22:32:26 +0800

    完成丙級第三題術科
    多邊形影像地圖太麻煩了，直接參考別人拉完的座標

---
+++ ClassC/WebC03/JavaScript.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $('li img').mouseover(function () {
+        var bind = $(this).data('bind');
+        $(this).attr('src', bind);
+    }).mouseout(function () {
+        var show = $(this).data('show');
+        $(this).attr('src', show);
+    });
+});
